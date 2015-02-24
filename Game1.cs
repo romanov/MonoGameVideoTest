df5c36bb209ddfced97ffa9249fe8442d8a885a9
romanov <noreply@github.io>
2015-02-24 20:11:55 +0300

    Initial

---
+++ Game1.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
+namespace VideoTestGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+
+        private VideoPlayer player;
+        private Video video;
+        private Texture2D videoTexture;
+
+        public Game1(): base()
+        {
+            Content.RootDirectory = "Content";
+
+            Screen screen = Screen.AllScreens.First(e => e.Primary);
+            Window.IsBorderless = true;
+            Window.Position = new Point(screen.Bounds.X, screen.Bounds.Y);
+
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = screen.Bounds.Width,
+                PreferredBackBufferHeight = screen.Bounds.Height,
+                IsFullScreen = false
+            };
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            video = Content.Load<Video>("space");
+            player = new VideoPlayer();
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (player.State == MediaState.Stopped)
+                player.Play(video);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Only call GetTexture if a video is playing or paused
+            if (player.State != MediaState.Stopped)
+                videoTexture = player.GetTexture();
+
+            // Drawing to the rectangle will stretch the 
+            // video to fill the screen
+            Rectangle screen = new Rectangle(GraphicsDevice.Viewport.X,
+                GraphicsDevice.Viewport.Y,
+                GraphicsDevice.Viewport.Width,
+                GraphicsDevice.Viewport.Height);
+
+            // Draw the video, if we have a texture to draw.
+            if (videoTexture != null)
+            {
+                spriteBatch.Begin();
+                spriteBatch.Draw(videoTexture, screen, Color.White);
+                spriteBatch.End();
+            }
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
